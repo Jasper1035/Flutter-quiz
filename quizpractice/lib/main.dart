0537dd845e88223df75d33205eaa0b948c586e62
Jasper1035 <104673426+Jasper1035@users.noreply.github.com>
2026-03-16 19:38:27 +0000

    start screen done

--- quizpractice/lib/main.dart
+++ quizpractice/lib/main.dart
@@ -10,6 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: MainPage());
+    return MaterialApp(debugShowCheckedModeBanner: false, home: MainPage());
   }
 }
--- quizpractice/lib/main_page.dart
+++ quizpractice/lib/main_page.dart
@@ -6,8 +6,33 @@ class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color.fromARGB(39, 0, 0, 0),
       appBar: AppBar(title: Text('Flutter Quiz')),
-      body: Center(child: Container(child: Text('Flutter Quiz'))),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset(
+              'assets/quiz-logo.png',
+              width: 300,
+              color: const Color.fromARGB(110, 255, 193, 7),
+            ),
+            SizedBox(height: 40),
+            OutlinedButton.icon(
+              onPressed: () {},
+              style: OutlinedButton.styleFrom(
+                foregroundColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                side: BorderSide(width: 2, color: Colors.white),
+              ),
+              label: Text('Start Quiz'),
+              icon: Icon(Icons.arrow_forward),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
