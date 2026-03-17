aa13aa20a4b43cf46d126079c984b4558064d8cf
Jasper1035 <104673426+Jasper1035@users.noreply.github.com>
2026-03-17 20:26:27 +0000

    link the start screen with the questions  and showed  the questions on the display

--- quizpractice/lib/quiz.dart
+++ quizpractice/lib/quiz.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quizpractice/data/questions.dart';
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
@@ -8,8 +9,28 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
+  var currentQuestionIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final currentQuestion = questions[currentQuestionIndex];
+
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(currentQuestion.text, style: TextStyle(color: Colors.red)),
+              SizedBox(height: 10),
+              ...currentQuestion.answer.map((answer) {
+                return ElevatedButton(onPressed: () {}, child: Text(answer));
+              }),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
